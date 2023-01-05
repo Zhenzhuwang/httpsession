10a56d11a2fe98246b908441f3fd3ae231da7215
zhenzhu <wangzhenzhu37@gmail.com>
2023-01-05 15:07:18 +0800

    httpsession example

---
+++ src/test/java/sg/edu/us/iss/app/httpsession/HttpsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.us.iss.app.httpsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
